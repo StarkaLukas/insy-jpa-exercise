8c82cd09dd0c2399b3f84829c5b45061ebde44bb
StarkaLukas <l.starka@students.htl-leonding.ac.at>
2021-04-04 20:36:51 +0200

    write IntTestBase

---
+++ src/test/java/at/htl/IntTestBase.java
@@ -0,0 +1,25 @@
+package at.htl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import javax.inject.Inject;
+import javax.transaction.NotSupportedException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
+
+public class IntTestBase {
+
+    @Inject
+    protected UserTransaction transaction;
+
+    @BeforeEach
+    public void beginTransaction() throws SystemException, NotSupportedException {
+        transaction.begin();
+    }
+
+    @AfterEach
+    public void rollbackTransaction() throws SystemException {
+        transaction.rollback();
+    }
+}
