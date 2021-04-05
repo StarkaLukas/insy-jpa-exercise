6003917eac87accbefe9ce14fc39203a6f29983e
StarkaLukas <l.starka@students.htl-leonding.ac.at>
2021-04-05 18:32:40 +0200

    implement MovieResource

---
+++ src/main/java/at/htl/api/MovieResource.java
@@ -0,0 +1,52 @@
+package at.htl.api;
+
+import at.htl.model.movie.MovieDTO;
+import at.htl.workloads.movie.Movie;
+import at.htl.workloads.movie.MovieService;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/api/movie")
+public class MovieResource {
+
+    private final MovieService movieService;
+
+    public MovieResource(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GET
+    @Path("all")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getAllMovies() {
+        List<Movie> movies = movieService.getAllMovies();
+
+        return Response.ok(movies).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getMovie(@PathParam("id") Long id) {
+        Movie movie = movieService.getMovieById(id);
+        boolean movieWasFound = movie != null;
+
+        return (movieWasFound ? Response.ok(movie) : Response.status(Response.Status.BAD_REQUEST)).build();
+    }
+
+    @Transactional
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addMovie(MovieDTO newMovie) {
+        boolean result = movieService.addMovie(newMovie);
+
+        return (result ? Response.ok(newMovie) : Response.status(Response.Status.BAD_REQUEST)).build();
+    }
+}
