52f62861e26473106b2b87d16dd43cac5af524e5
StarkaLukas <l.starka@students.htl-leonding.ac.at>
2021-04-05 23:27:12 +0200

    implement addStore method

--- src/main/java/at/htl/workloads/store/StoreServiceImpl.java
+++ src/main/java/at/htl/workloads/store/StoreServiceImpl.java
@@ -1,7 +1,11 @@
 package at.htl.workloads.store;
 
+import at.htl.model.sale.SaleDTO;
 import at.htl.model.staff.StaffDTO;
 import at.htl.model.store.StoreDTO;
+import at.htl.workloads.customer.Customer;
+import at.htl.workloads.movie.Movie;
+import at.htl.workloads.sale.Sale;
 import at.htl.workloads.staff.Staff;
 
 import javax.enterprise.context.RequestScoped;
@@ -31,19 +35,49 @@ public class StoreServiceImpl implements StoreService{
         }
 
         Store store = new Store();
-        store.setId(newStore.getId());
         store.setCity(newStore.getCity());
         store.setName(newStore.getName());
 
-//        List<Staff> staffList = new ArrayList<>();
-//
-//        for (StaffDTO staff : newStore.getStaffList()) {
-//            Staff newStaff = new Staff();
-//            newStaff.setId(staff.getId());
-//            newStaff.setStore(store);
-//            newStaff.setSalary(staff.getSalary());
-//            newStaff.setSales(staff.getS);
-//        }
+        List<Staff> staffList = new ArrayList<>();
+
+        for (StaffDTO staff : newStore.getStaffList()) {
+            Staff newStaff = new Staff();
+            newStaff.setStore(store);
+            newStaff.setSalary(staff.getSalary());
+            newStaff.setFirstName(staff.getFirstName());
+            newStaff.setLastName(staff.getLastName());
+            newStaff.setHireDate(staff.getHireDate());
+
+            for (SaleDTO sale : staff.getSales()) {
+                Sale newSale = new Sale();
+                newSale.setStaff(newStaff);
+                newSale.setPayment_date(sale.getPayment_date());
+                newSale.setPrice(sale.getPrice());
+
+                Customer customer = new Customer();
+                customer.setDateOfBirth(sale.getCustomer().getDateOfBirth());
+                customer.setZip(sale.getCustomer().getZip());
+                customer.setFirstName(sale.getCustomer().getFirstName());
+                customer.setLastName(sale.getCustomer().getLastName());
+                customer.setStreet(sale.getCustomer().getStreet());
+                customer.setPlace(sale.getCustomer().getPlace());
+                customer.getSales().add(newSale);
+
+                Movie movie = new Movie();
+                movie.setLanguage(sale.getMovie().getLanguage());
+                movie.setDuration(sale.getMovie().getDuration());
+                movie.setRating(sale.getMovie().getRating());
+                movie.setYear(sale.getMovie().getYear());
+                movie.setTitle(sale.getMovie().getTitle());
+                movie.getSales().add(newSale);
+
+                newSale.setCustomer(customer);
+                newSale.setMovie(movie);
+                newStaff.getSales().add(newSale);
+            }
+
+            store.getStaffList().add(newStaff);
+        }
 
         storeRepository.addStore(store);
 
