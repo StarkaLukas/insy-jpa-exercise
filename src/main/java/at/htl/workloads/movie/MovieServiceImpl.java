2eaaff0f657060f86d12664a9b210b0f97f9fbc6
StarkaLukas <l.starka@students.htl-leonding.ac.at>
2021-04-05 23:08:19 +0200

    implement addMovie method

--- src/main/java/at/htl/workloads/movie/MovieServiceImpl.java
+++ src/main/java/at/htl/workloads/movie/MovieServiceImpl.java
@@ -1,6 +1,11 @@
 package at.htl.workloads.movie;
 
 import at.htl.model.movie.MovieDTO;
+import at.htl.model.sale.SaleDTO;
+import at.htl.workloads.customer.Customer;
+import at.htl.workloads.sale.Sale;
+import at.htl.workloads.staff.Staff;
+import at.htl.workloads.store.Store;
 
 import javax.enterprise.context.RequestScoped;
 import java.util.List;
@@ -28,12 +33,57 @@ public class MovieServiceImpl implements MovieService {
         }
 
         Movie movie = new Movie();
-        movie.setId(newMovie.getId());
         movie.setDuration(newMovie.getDuration());
         movie.setLanguage(newMovie.getLanguage());
         movie.setRating(newMovie.getRating());
         movie.setTitle(newMovie.getTitle());
         movie.setYear(newMovie.getYear());
+
+        System.out.println(movie);
+
+        for (SaleDTO saleDTO : newMovie.getSaleList()) {
+            Sale sale = new Sale();
+            sale.setMovie(movie);
+            sale.setPrice(saleDTO.getPrice());
+            sale.setPayment_date(saleDTO.getPayment_date());
+
+            System.out.println(sale);
+
+            Customer customer = new Customer();
+            customer.setStreet(saleDTO.getCustomer().getStreet());
+            customer.setPlace(saleDTO.getCustomer().getPlace());
+            customer.setFirstName(saleDTO.getCustomer().getFirstName());
+            customer.setLastName(saleDTO.getCustomer().getLastName());
+            customer.setZip(saleDTO.getCustomer().getZip());
+            customer.setDateOfBirth(saleDTO.getCustomer().getDateOfBirth());
+            customer.getSales().add(sale);
+
+            System.out.println(customer);
+
+            sale.setCustomer(customer);
+
+            Staff staff = new Staff();
+            staff.setHireDate(saleDTO.getStaff().getHireDate());
+            staff.setFirstName(saleDTO.getStaff().getFirstName());
+            staff.setLastName(saleDTO.getStaff().getLastName());
+            staff.setSalary(saleDTO.getStaff().getSalary());
+            staff.getSales().add(sale);
+
+            Store store = new Store();
+            store.setCity(saleDTO.getStaff().getStore().getCity());
+            store.setName(saleDTO.getStaff().getStore().getName());
+            store.getStaffList().add(staff);
+
+            staff.setStore(store);
+
+            sale.setStaff(staff);
+            sale.setStaff(staff);
+
+            movie.getSales().add(sale);
+
+            System.out.println(movie);
+        }
+
         movieRepository.addMovie(movie);
 
         return true;
